e416a81b43ddc2476f6cd00d0b93f125acc1e1ea
sristiacharya13 <sristiacharya18@gmail.com>
2025-02-15 21:00:26 +0530

    added

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+// import React from "react";
 import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-black pt-10 md:pt-0">
-      {/* Rotating Cube */}
+    <div className="flex items-center justify-center w-full h-screen bg-black">
       <motion.div
-        className="relative w-[450px] h-[400px] max-w-full"
+        className="relative w-[450px] h-[350px]"
         animate={{
           rotateX: [0, 30, 0, -30, 0],
           rotateY: [0, 45, 45, -45, -45],
@@ -22,36 +21,25 @@ const Hero = () => {
         ].map((face, index) => (
           <div
             key={index}
-            className="absolute w-[450px] h-[400px] bg-white border border-gray-300 flex items-center justify-center text-white text-xl font-bold"
-            style={{ transform: face.transform }}
+            className="absolute w-[450px] h-[350px] flex items-center justify-center text-black text-xl font-bold border-gray-400"
+            style={{ 
+              transform: face.transform,
+              backgroundColor: 'rgba(190,190,190,0.8)', // Silver color with 30% opacity
+            }}
           >
             <div className="flex flex-col absolute top-10 left-10 items-center text-center">
-              <img
-                src="https://cdn.prod.website-files.com/67092b02e0a47e061ff6aef7/67094393682de8d96652a577_logo.svg"
-                alt="U Digital Studio Logo"
-                className="h-24 w-auto mb-5"
-              />
+              <img  className="w-30 h-20"
+              src="src/assets/favicon[1].png" alt="" />
             </div>
-            <div className="absolute bottom-10 left-8 text-8xl font-bold text-black">
-              DIGITAL <br /> STUDIO
+            <div className="mt-4 absolute top-25 left-8 text-6xl font-bold text-black">
+              COGNIMUSE
+            </div>
+            <div className="mt-7 absolute bottom-30 left-8 text-xl font-bold text-black">
+              Simplifying Experiences. Amplifying Impacts
             </div>
           </div>
         ))}
       </motion.div>
-
-      {/* New Text Section */}
-      <div className="flex justify-between items-center w-full max-w-screen-xl px-1 text-white mt-12">
-        <p className="text-left text-sm sm:text-base md:text-xs w-1/3">  
-          READY TO FIND YOUR NEXT IDEAL  
-          <br /> TEMPLATE  
-        </p>
-        <p className="text-center text-sm sm:text-base md:text-xs w-1/3">  
-          UPS <br /> DIGITAL STUDIO  
-        </p>
-        <p className="text-right text-sm sm:text-base md:text-xs w-1/3">  
-          MADE TO LEAVE A MARK.  
-        </p>
-      </div>
     </div>
   );
 };
